5b4a652fb5fff5e0ee167b3db30303562bc9ebe3
pycorn0729 <hayesdominique0729@gmail.com>
2026-03-15 10:23:31 +0100

    chore: update allowedProxiedAccount constant in StakeWrapConstants and add proxy address to documentation

--- contracts/StakeWrapConstants.sol
+++ contracts/StakeWrapConstants.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.3;
 contract StakeWrapConstants {
     // Predefined SS58 coldkey address: 5HCT4AarReToT1BKyLtJXJfSLs4zRS7dENnZ7iysqrqxXyV7
     bytes32 public constant allowedColdkey = 0xe3154da4f09419591350683863465fe94568b34952c139e0fc2119c1ab64bdf9;
-    // Proxied account (destination for transferToProxiedAccount; source for pullFromProxiedAccount). Set to same as allowedColdkey by default.
-    bytes32 public constant allowedProxiedAccount = 0xe3154da4f09419591350683863465fe94568b34952c139e0fc2119c1ab64bdf9;
+    // Proxied account (destination for transferToProxiedAccount; source for pullFromProxiedAccount). SS58: 5FptUDrtvf6y4GmQKekEPmELeSC5MsLpRRDPFNXmHmCwfbs3
+    bytes32 public constant allowedProxiedAccount = 0xa6673984bb4f39d185a00d730c2b31cd41c2ff97760a3e3cc14d123875d91f68;
     // XOR key for obfuscating uint256 parameters
     uint256 internal constant XOR_KEY = 0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef;
 }
