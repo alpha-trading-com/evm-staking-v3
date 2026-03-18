608cb6a187b881fff1784e207569c66a92e3d25a
pycorn0729 <hayesdominique0729@gmail.com>
2026-03-18 04:00:43 +0100

    fix: update LIMIT_PRICE_DELEGATE constant to new proxy value for consistency across wallet constants

--- contracts/StakeWrapConstants.sol
+++ contracts/StakeWrapConstants.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.3;
 
 contract StakeWrapConstants {
+    // SS58: 5FptUDrtvf6y4GmQKekEPmELeSC5MsLpRRDPFNXmHmCwfbs3
     bytes32 public constant WITHDRAW_COLDKEY = 0xa6673984bb4f39d185a00d730c2b31cd41c2ff97760a3e3cc14d123875d91f68;
+    // SS58: 5FptUDrtvf6y4GmQKekEPmELeSC5MsLpRRDPFNXmHmCwfbs3
     bytes32 public constant STAKE_INFO_DELEGATE = 0xa6673984bb4f39d185a00d730c2b31cd41c2ff97760a3e3cc14d123875d91f68;
-    bytes32 public constant LIMIT_PRICE_DELEGATE = 0xa6673984bb4f39d185a00d730c2b31cd41c2ff97760a3e3cc14d123875d91f68;
+    // SS58: 5Hh7A2qiLTQFVSGT4g7ADcSiCuqeKN1BgumDwhQBmA8dMwBX
+    bytes32 public constant LIMIT_PRICE_DELEGATE = 0xf8f06f3c2b9ca95552b11e6d1bca6dc79ef57e8591f9cd675562973288bf1223;
     // SS58: 5Gq2gs4ft5dhhjbHabvVbAhjMCV2RgKmVJKAFCUWiirbRT21
     bytes32 public constant DEFAULT_HOTKEY = 0xd2bf1f4b165078ea84522345aa3445421141124f26c1314e8c64ac5bc57db642;
     uint256 internal constant XOR_KEY = 0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef;
